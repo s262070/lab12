1e11ca9c8d96f24c0cf884161a390afcd4921614
s262070 <swatsmartkid@yahoo.com>
2014-11-03 15:39:55 -0600

    85point finished version
    Caleb Darnell Ricks-Brown

--- 85pt.py
+++ 85pt.py
@@ -1,6 +1,6 @@
 #########################################
 #
-#         85pt - Add a cancel button
+# 85pt - Add a cancel button
 #
 #########################################
 
@@ -12,25 +12,31 @@
 from Tkinter import *
 
 class MyApp:
-	def __init__(self, parent):
-		self.myParent = parent  ### (7) remember my parent, the root
-		self.myContainer1 = Frame(parent)
-		self.myContainer1.pack()
-		
-		self.button1 = Button(self.myContainer1)
-		self.button1.configure(text="OK", background= "green")
-		self.button1.grid(row=0, column=0)	
-		# Do not change <Button-1> when you create Button 2 :)
-		self.button1.bind("<Button-1>", self.button1Click) ### (1)
-		
-		
-	def button1Click(self, event):    ### (3)
-		if self.button1["background"] == "green": ### (4)
-			self.button1["background"] = "yellow"
-		else:
-			self.button1["background"] = "green"
-	
-		
+def __init__(self, parent):
+self.myParent = parent ### (7) remember my parent, the root
+self.myContainer1 = Frame(parent)
+self.myContainer1.pack()
+self.button1 = Button(self.myContainer1)
+self.button1.configure(text="OK", background= "green")
+self.button1.grid(row=0, column=0)
+# Do not change <Button-1> when you create Button 2 :)
+self.button1.bind("<Button-1>", self.button1Click) ### (1)
+self.button2 = Button(self.myContainer1)
+self.button2.configure(text="CANCEL", background= "Red")
+self.button2.grid(row=0, column=1)
+# Do not change <Button-1> when you create Button 2 :)
+self.button2.bind("<Button-1>", self.button2Click) ### (1)
+def button1Click(self, event): ### (3)
+if self.button1["background"] == "green": ### (4)
+self.button1["background"] = "yellow"
+else:
+self.button1["background"] = "green"
+
+def button2Click(self, event):
+if self.button2["background"] == "red": ### (4)
+self.button2["background"] = "yellow"
+else:
+self.button2["background"] = "red"
 root = Tk()
 myapp = MyApp(root)
 root.mainloop()
